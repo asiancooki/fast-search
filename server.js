c93347287d8b694726709a8c02a0433d71f5a6b3
asiancooki <chauyauhong@gmail.com>
2025-05-08 02:50:01 -0400

    Initial commit 2

--- server.js
+++ server.js
@@ -71,5 +71,5 @@ app.post('/search', async (req, res) => {
     }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
